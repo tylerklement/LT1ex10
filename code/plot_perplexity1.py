137a18e71808173b66afe517ddaea4dd350404ff
violinonero <violinonero@hotmail.it>
2016-01-28 12:36:59 +0100

    Update plot_perplexity1.py

--- code/plot_perplexity1.py
+++ code/plot_perplexity1.py
@@ -1,6 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import matplotlib.image as mpimg
 
 input_neuron = []
 perplexity = []
@@ -18,17 +16,23 @@ perplexity_dev = perplexity[0:7]
 input_neuron_test = input_neuron[7:]
 perplexity_test = perplexity[7:]
 
+plt.figure(figsize=(12, 6))
 plt.plot(input_neuron_dev, perplexity_dev, color='cornflowerblue', linewidth=2, label="Development Set")
 plt.plot(input_neuron_test, perplexity_test, color='indianred',  linewidth=2, label="Test Set")
 
-red_patch = mpatches.Patch(color='cornflowerblue', label="Development Set")
-blue_patch = mpatches.Patch(color='indianred', label="Test Set")
-
-plt.legend(handles=[red_patch, blue_patch], loc=1)
 plt.xlabel('Input Neurons')
 plt.ylabel('Perplexity')
 
+# Place a legend to the right of this smaller figure.
+plt.legend(loc=1)
+
+
+#plt.show()
 plt.savefig('plot_perplexity.png')
+
+
+
+
 
 
 
